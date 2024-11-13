9deac832829709f06547d23b72c8cf821e507abc
Leorgs2675 <163139274+Leorgs2675@users.noreply.github.com>
2024-11-13 19:08:20 +0700

    thanh cong

---
+++ Vue_B2/src/BTVN_B2.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const dsSp = ref([
+    "Laptop",
+    "Dien Thoai",
+    "Tai Nghe",
+    "May Tinh Bang"
+])
+const SanPham = ref(
+    {
+        name:"Laptop",
+        price: 500,
+        des:"window"
+    }
+)
+const NhanVien = ref([
+    {
+        name:"Nguyen Van A",
+        position:"Ke Toan"
+    },
+    {
+        name:"Nguyen Van B",
+        position:"Nhan Vien"
+    },
+    {
+        name:"Nguyen Van C",
+        position:"Thu Ky"
+    },
+    {
+        name:"Nguyen Van D",
+        position:"Quan Ly"
+    }
+])
+const Xoa =(index)=>{
+    NhanVien.value.splice(index,1)
+}
+const number = ref(0)
+const Tang=()=>{
+    number.value++
+}
+const Giam=()=>{
+    number.value--
+}
+const User = reactive({
+    name:"Nguyen Van A",
+    age: 20
+})
+const ChangesUser=()=>{
+    User.name = "Nguyen Van B",
+    User.age = 100
+}
+const tasks = reactive([
+    "Di cho",
+    "Lam bt",
+    "Don nha"
+])
+</script>
+<template>
+<h1>BTVN</h1>
+---------------------------
+<h5>B1:</h5>
+<ul>
+    <li v-for="(sp, index) in dsSp" :key="index">
+        {{ sp }}
+    </li>
+</ul>
+---------------------------
+<h5>B2:</h5>
+<ul>
+    <li v-for="(value ,key) in SanPham" :key="key">
+        {{ key }} : {{ value }}
+    </li>
+</ul>
+---------------------------
+<h5>B3:</h5>
+<div v-for="(nv,index) in NhanVien" :key="nv.name">
+{{ nv.name }} {{ nv.position }} <button @click="Xoa">Xoa</button>
+</div>
+---------------------------
+<h5>B4:</h5>
+<p>{{ number }}</p>
+<button @click="Tang">+</button>
+<button @click="Giam">-</button><br>
+---------------------------
+<h5>B5:</h5>
+<div>
+    {{ User.name }} {{ User.age }}
+</div>
+<button @click="ChangesUser" >Thay doi</button><br>
+---------------------------
+<h5>B6:</h5>
+<div v-for="(t,index) in tasks" :key="index">
+    {{ t }}
+</div>
+</template>
+<style>
+</style>
--- Vue_B2/src/main.js
+++ Vue_B2/src/main.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './BTVN_B2.vue'
 
 
 const app = createApp(App)
