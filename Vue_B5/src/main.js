58e792061ee3fc93fe5736f87e49d65bf2549f1c
Leorgs2675 <163139274+Leorgs2675@users.noreply.github.com>
2024-11-18 22:10:22 +0700

    CRUD ADD & DELETE VOI LIST FIX CUNG

--- Vue_B5/src/main.js
+++ Vue_B5/src/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './BTVN_B9.vue'
 
 createApp(App).mount('#app')
